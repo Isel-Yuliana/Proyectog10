1a39d95252cfe52b2f7902017c3dd41b658820ad
Isel-Yuliana <210435@unsaac.edu.pe>
2025-06-09 01:07:26 -0500

    Agregado CRUD de usuarios y clientes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const cors = require('cors');
 require('./db/connection');
 const productosRoutes = require('./routes/productos.routes');
+const usuariosRoutes = require('./routes/usuarios.routes');
+const clientesRoutes = require('./routes/clientes.routes');
 
 const app = express();
 
@@ -11,6 +13,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/productos', productosRoutes);
+app.use('/api/usuarios', usuariosRoutes);
+app.use('/api/clientes', clientesRoutes);
 
 
 // Ruta base
